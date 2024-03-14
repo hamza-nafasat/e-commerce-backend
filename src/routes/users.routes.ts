2829bc92151ee08e955acf1fbdf899a3a55b31ed
Hamza Nafasat <149063128+hamza-nafasat@users.noreply.github.com>
2024-03-14 20:29:52 +0500

    init files

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,21 @@
+import express, { Express } from "express";
+import {
+	createUser,
+	deleteUser,
+	getAllUsers,
+	getSingleUser,
+} from "../controllers/users.controllers.js";
+import { isAdmin } from "../middlewares/auth.js";
+
+const app: Express = express();
+
+// ADMIN ONLY === get all users
+app.get("/all", isAdmin, getAllUsers);
+
+// register or signup route
+app.post("/new", createUser);
+
+// get one user or delete one user
+app.route("/one/:_id").get(getSingleUser).delete(deleteUser);
+
+export default app;
---
+++ src/types/schema.types.ts
@@ -0,0 +1,50 @@
+export interface UserSchemaTypes {
+	_id: string;
+	name: string;
+	email: string;
+	dob: Date;
+	photo: string;
+	gender: "male" | "female";
+	role: "admin" | "user";
+	createdAt?: Date;
+	updatedAt?: Date;
+	age: Date;
+}
+
+export interface ProductSchemaTypes {
+	name: string;
+	price: number;
+	stock: number;
+	photo: string;
+	category: string;
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+export interface OrderSchemaTypes {
+	userId: string;
+	subTotal: number;
+	tax: number;
+	shippingCharges: number;
+	discount: number;
+	total: number;
+	status: string;
+	createdAt?: Date;
+	updatedAt?: Date;
+	shippingInfo: {
+		address: string;
+		city: string;
+		state: string;
+		country: string;
+		pinCode: number;
+	};
+	orderItem: [
+		{
+			name: string;
+			photo: string;
+			price: number;
+			Quantity: number;
+			productId: string;
+		}
+	];
+}
