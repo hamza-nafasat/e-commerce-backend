54227dac0119c2571edd1a935532178b495f8d34
hamza-nafasat <hamzanafasat4155@gmail.com>
2024-03-27 20:30:50 +0500

    change order create controller and order model photo value change

--- src/controllers/orders.controllers.ts
+++ src/controllers/orders.controllers.ts
@@ -6,47 +6,61 @@ import Order from "../models/orders.model.js";
 import { newOrderReqTypes } from "../types/apis.types.js";
 import CustomError from "../utils/customClass.js";
 import { invalidateNodeCash, reduceStock, responseFunc } from "../utils/features.js";
+import Product from "../models/products.model.js";
 
 // =========================================
 // http://localhost:8000/api/v1/orders/new =  CREATE NEW ORDER
 // =========================================
 
 export const newOrderCreate = TryCatch(async (req: Request<{}, {}, newOrderReqTypes>, res, next) => {
-	const {
-		orderItem,
-		shippingInfo,
-		shippingCharges,
-		discount = 0,
-		subTotal,
-		tax = 0,
-		total,
-		userId,
-	} = req.body;
-	//// ensuring that all required fields are given
-	if (!orderItem || !shippingCharges || !shippingInfo || !subTotal || !total || !userId) {
-		return next(new CustomError("Please Provide All Fields", 400));
-	}
-	//// creating a order
-	const order = await Order.create({
-		orderItem,
-		shippingInfo,
-		shippingCharges,
-		discount,
-		subTotal,
-		tax,
-		total,
-		userId,
-	});
-	//// reducing stock from products bcz some items ordered
-	await reduceStock(orderItem);
-	//// invalidate cash items bcz of order
-	invalidateNodeCash({
-		isProducts: true,
-		isOrders: true,
-		isAdmins: true,
-		productId: order.orderItem.map((item) => String(item.productId)),
-	});
-	responseFunc(res, "Your Order Placed Successfully", 201);
+    const {
+        orderItem,
+        shippingInfo,
+        shippingCharges,
+        discount = 0,
+        subTotal,
+        tax = 0,
+        total,
+        userId,
+    } = req.body;
+    //// ensuring that all required fields are given
+    if (!orderItem || !shippingCharges || !shippingInfo || !subTotal || !total || !userId) {
+        return next(new CustomError("Please Provide All Fields", 400));
+    }
+
+    //// checking if all products are valid
+    for (let item of orderItem) {
+        if (!isValidObjectId(item.productId)) {
+            return next(new CustomError("Invalid Product Id", 400));
+        }
+        const product = await Product.findById(item.productId).select("stock name");
+        if (!product) return next(new CustomError("Invalid Product Id", 400));
+        //// checking if product stock is enough
+        if (item.Quantity > product.stock) {
+            return next(new CustomError(`Insufficient Stock Available For ${product.name}`, 400));
+        }
+    }
+    //// creating a order
+    const order = await Order.create({
+        orderItem,
+        shippingInfo,
+        shippingCharges,
+        discount,
+        subTotal,
+        tax,
+        total,
+        userId,
+    });
+    //// reducing stock from products bcz some items ordered
+    await reduceStock(orderItem);
+    //// invalidate cash items bcz of order
+    invalidateNodeCash({
+        isProducts: true,
+        isOrders: true,
+        isAdmins: true,
+        productId: order.orderItem.map((item) => String(item.productId)),
+    });
+    responseFunc(res, "Your Order Placed Successfully", 201);
 });
 
 // =========================================
@@ -54,17 +68,17 @@ export const newOrderCreate = TryCatch(async (req: Request<{}, {}, newOrderReqTy
 // =========================================
 
 export const getMyOrders = TryCatch(async (req, res, next) => {
-	const { id } = req.query;
-	let nodeCashKey = `my-orders-${id}`;
-	let myOrders = [];
-	if (nodeCash.has(nodeCashKey)) {
-		myOrders = JSON.parse(nodeCash.get(nodeCashKey) as string);
-	} else {
-		myOrders = await Order.find({ user: id });
-		if (!myOrders) return next(new CustomError("Invalid key or Orders Not Found", 404));
-		nodeCash.set(nodeCashKey, JSON.stringify(myOrders));
-	}
-	responseFunc(res, "", 200, { length: myOrders.length, myOrders });
+    const { id } = req.query;
+    let nodeCashKey = `my-orders-${id}`;
+    let myOrders = [];
+    if (nodeCash.has(nodeCashKey)) {
+        myOrders = JSON.parse(nodeCash.get(nodeCashKey) as string);
+    } else {
+        myOrders = await Order.find({ user: id });
+        if (!myOrders) return next(new CustomError("Invalid key or Orders Not Found", 404));
+        nodeCash.set(nodeCashKey, JSON.stringify(myOrders));
+    }
+    responseFunc(res, "", 200, { length: myOrders.length, myOrders });
 });
 
 // =========================================
@@ -72,16 +86,16 @@ export const getMyOrders = TryCatch(async (req, res, next) => {
 // =========================================
 
 export const getAllOrders = TryCatch(async (req, res, next) => {
-	let nodeCashKey = "all-orders";
-	let allOrders = [];
-	if (nodeCash.has(nodeCashKey)) {
-		allOrders = JSON.parse(nodeCash.get(nodeCashKey) as string);
-	} else {
-		allOrders = await Order.find().populate("userId", "name");
-		if (!allOrders) return next(new CustomError("Orders Not Found", 404));
-		nodeCash.set(nodeCashKey, JSON.stringify(allOrders));
-	}
-	responseFunc(res, "", 200, { length: allOrders.length, allOrders });
+    let nodeCashKey = "all-orders";
+    let allOrders = [];
+    if (nodeCash.has(nodeCashKey)) {
+        allOrders = JSON.parse(nodeCash.get(nodeCashKey) as string);
+    } else {
+        allOrders = await Order.find().populate("userId", "name");
+        if (!allOrders) return next(new CustomError("Orders Not Found", 404));
+        nodeCash.set(nodeCashKey, JSON.stringify(allOrders));
+    }
+    responseFunc(res, "", 200, { length: allOrders.length, allOrders });
 });
 
 // ================================================
@@ -89,65 +103,65 @@ export const getAllOrders = TryCatch(async (req, res, next) => {
 // ================================================
 
 export const getSingleOrder = TryCatch(async (req, res, next) => {
-	const { orderId } = req.params;
-	if (!isValidObjectId(orderId)) {
-		return next(new CustomError("Invalid Order Id", 400));
-	}
-	let nodeCashKey = `order-${orderId}`;
-	let order;
-	if (nodeCash.has(nodeCashKey)) {
-		order = JSON.parse(nodeCash.get(nodeCashKey) as string);
-	} else {
-		order = await Order.findById(orderId).populate("userId", "name");
-		if (!order) return next(new CustomError("Order Not Found", 404));
-		nodeCash.set(nodeCashKey, JSON.stringify(order));
-	}
-	responseFunc(res, "", 200, order);
+    const { orderId } = req.params;
+    if (!isValidObjectId(orderId)) {
+        return next(new CustomError("Invalid Order Id", 400));
+    }
+    let nodeCashKey = `order-${orderId}`;
+    let order;
+    if (nodeCash.has(nodeCashKey)) {
+        order = JSON.parse(nodeCash.get(nodeCashKey) as string);
+    } else {
+        order = await Order.findById(orderId).populate("userId", "name");
+        if (!order) return next(new CustomError("Order Not Found", 404));
+        nodeCash.set(nodeCashKey, JSON.stringify(order));
+    }
+    responseFunc(res, "", 200, order);
 });
 
 export const processSingleOrder = TryCatch(async (req, res, next) => {
-	const { orderId } = req.params;
-	if (!isValidObjectId(orderId)) {
-		return next(new CustomError("Invalid Order Id", 400));
-	}
+    const { orderId } = req.params;
+    if (!isValidObjectId(orderId)) {
+        return next(new CustomError("Invalid Order Id", 400));
+    }
 
-	let order = await Order.findById(orderId).select("status userId");
-	if (!order) return next(new CustomError("Order Not Found", 404));
-	//// if orders found the process them according their firs status
-	if (order.status === "processing") {
-		order.status = "shipped";
-	} else if (order.status === "shipped") {
-		order.status = "delivered";
-	} else {
-		return next(new CustomError("Order Already Delivered", 400));
-	}
+    let order = await Order.findById(orderId).select("status userId");
+    if (!order) return next(new CustomError("Order Not Found", 404));
+    //// if orders found the process them according their firs status
+    if (order.status === "processing") {
+        order.status = "shipped";
+    } else if (order.status === "shipped") {
+        order.status = "delivered";
+    } else {
+        return next(new CustomError("Order Already Delivered", 400));
+    }
 
-	await order.save();
-	//// invalidate nodeCash after updating a order
-	invalidateNodeCash({
-		isProducts: false,
-		isOrders: true,
-		isAdmins: true,
-		userId: order.userId,
-		orderId: order._id.toString(),
-	});
-	responseFunc(res, "", 200, order);
+    await order.save();
+    //// invalidate nodeCash after updating a order
+    invalidateNodeCash({
+        isProducts: false,
+        isOrders: true,
+        isAdmins: true,
+        userId: order.userId,
+        orderId: order._id.toString(),
+    });
+    responseFunc(res, "", 200, order);
 });
 
 export const deleteSingleOrder = TryCatch(async (req, res, next) => {
-	const { orderId } = req.params;
-	if (!isValidObjectId(orderId)) {
-		return next(new CustomError("Invalid Order Id", 400));
-	}
-	let order = await Order.findByIdAndDelete(orderId);
-	if (!order) return next(new CustomError("Order Already Deleted", 404));
-	//// invalidate nodeCash after deleting a order
-	invalidateNodeCash({
-		isProducts: false,
-		isOrders: true,
-		isAdmins: true,
-		userId: order.userId,
-		orderId: order._id.toString(),
-	});
-	responseFunc(res, "Order Deleted Successfully", 200);
+    const { orderId } = req.params;
+    if (!isValidObjectId(orderId)) {
+        return next(new CustomError("Invalid Order Id", 400));
+    }
+    let order = await Order.findByIdAndDelete(orderId);
+    if (!order) return next(new CustomError("Order Already Deleted", 404));
+    //// invalidate nodeCash after deleting a order
+    invalidateNodeCash({
+        isProducts: false,
+        isOrders: true,
+        isAdmins: true,
+        userId: order.userId,
+        orderId: order._id.toString(),
+    });
+    responseFunc(res, "Order Deleted Successfully", 200);
 });
--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -2,73 +2,76 @@ import { Schema, Types, model } from "mongoose";
 import { OrderSchemaTypes } from "../types/schema.types.js";
 
 const singleOrderItem = {
-	name: String,
-	photo: String,
-	price: Number,
-	Quantity: Number,
-	productId: {
-		type: Types.ObjectId,
-		ref: "Product",
-	},
+    name: String,
+    photo: {
+        publicId: String,
+        url: String,
+    },
+    price: Number,
+    Quantity: Number,
+    productId: {
+        type: Types.ObjectId,
+        ref: "Product",
+    },
 };
 
 const orderSchema = new Schema<OrderSchemaTypes>(
-	{
-		shippingInfo: {
-			address: {
-				type: String,
-				required: true,
-			},
-			city: {
-				type: String,
-				required: true,
-			},
-			state: {
-				type: String,
-				required: true,
-			},
-			country: {
-				type: String,
-				required: true,
-			},
-			pinCode: {
-				type: Number,
-				required: true,
-			},
-		},
-		orderItem: [singleOrderItem],
-		userId: {
-			type: String,
-			ref: "User",
-			required: true,
-		},
-		subTotal: {
-			type: Number,
-			required: true,
-		},
-		tax: {
-			type: Number,
-			required: true,
-		},
-		discount: {
-			type: Number,
-			required: true,
-		},
-		total: {
-			type: Number,
-			required: true,
-		},
-		shippingCharges: {
-			type: Number,
-			required: true,
-		},
-		status: {
-			type: String,
-			enum: ["processing", "delivered", "shipped"],
-			default: "processing",
-		},
-	},
-	{ timestamps: true }
+    {
+        shippingInfo: {
+            address: {
+                type: String,
+                required: true,
+            },
+            city: {
+                type: String,
+                required: true,
+            },
+            state: {
+                type: String,
+                required: true,
+            },
+            country: {
+                type: String,
+                required: true,
+            },
+            pinCode: {
+                type: Number,
+                required: true,
+            },
+        },
+        orderItem: [singleOrderItem],
+        userId: {
+            type: String,
+            ref: "User",
+            required: true,
+        },
+        subTotal: {
+            type: Number,
+            required: true,
+        },
+        tax: {
+            type: Number,
+            required: true,
+        },
+        discount: {
+            type: Number,
+            required: true,
+        },
+        total: {
+            type: Number,
+            required: true,
+        },
+        shippingCharges: {
+            type: Number,
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ["processing", "delivered", "shipped"],
+            default: "processing",
+        },
+    },
+    { timestamps: true }
 );
 
 const Order = model<OrderSchemaTypes>("Order", orderSchema);
